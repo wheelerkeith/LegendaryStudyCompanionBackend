a5682f0f74515e05e94b4772e9ace5d2078072c2
JosueDOrtiz <josueworkmail@gmail.com>
2020-03-23 14:13:46 -0500

    .

---
+++ src/test/java/com/revature/DriverTest.java
@@ -0,0 +1,32 @@
+package com.revature;
+
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//
+//import org.hibernate.Session;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.runners.MockitoJUnitRunner;
+//
+//import com.revature.daos.UserDao;
+//import com.revature.models.User;
+//import com.revature.services.UserService;
+//
+//@RunWith(MockitoJUnitRunner.class)
+public class DriverTest {
+	
+//	@InjectMocks
+//	private UserService us;
+//	
+//	@Mock
+//	private UserDao ud;
+//	
+//	@Test
+//	public void testAddUser() {
+//		User user = new User("billybob@gmail.com", "1234", "billybob@gmail.com");
+//		
+//		assertEquals(0, ud.addUser(user));
+//	}
+}
